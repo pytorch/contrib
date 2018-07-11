51fdc3867b11b5d3e3dcbe13d8746ff8560ae101
Adam Paszke <adam.paszke@gmail.com>
2018-07-11 07:39:01 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,19 @@
+torch-contrib
+=============
+
+This repository contains reviewed implementations of ideas from recent machine learning papers.
+
+Installation
+============
+
+pip:
+
+.. code:: bash
+
+    pip install torchcontrib
+
+From source:
+
+.. code:: bash
+
+    python setup.py install
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+import shutil
+import sys
+from setuptools import setup, find_packages
+
+
+readme = open('README.rst').read()
+
+VERSION = '0.0.1'
+
+setup(
+    # Metadata
+    name='torchcontrib',
+    version=VERSION,
+    author='PyTorch Core Team and Contributors',
+    author_email='soumith@pytorch.org',
+    url='https://github.com/pytorch/contrib',
+    description='implementations of ideas from recent papers',
+    long_description=readme,
+    license='BSD',
+
+    # Package info
+    packages=find_packages(exclude=('test',)),
+
+    zip_safe=True,
+)
