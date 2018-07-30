dee40b0b4f8cc5cd332fd97fe9d10ca1fad47462
Pavel Izmailov <izmailovpavel@gmail.com>
2018-07-30 21:41:11 +0300

    fast-swa cyclical lr schedule example

---
+++ examples/swa/train_fastswa.py
@@ -0,0 +1,180 @@
+import argparse
+import os
+import sys
+import time
+import torch
+import torch.nn.functional as F
+from torchcontrib.optim.swa import SWA, bn_update
+import torchvision
+import models
+import utils
+import tabulate
+import math
+
+
+parser = argparse.ArgumentParser(description='SGD/SWA training')
+parser.add_argument('--dir', type=str, default=None, required=True, help='training directory (default: None)')
+
+parser.add_argument('--dataset', type=str, default='CIFAR10', help='dataset name (default: CIFAR10)')
+parser.add_argument('--data_path', type=str, default=None, required=True, metavar='PATH',
+                    help='path to datasets location (default: None)')
+parser.add_argument('--batch_size', type=int, default=128, metavar='N', help='input batch size (default: 128)')
+parser.add_argument('--num_workers', type=int, default=4, metavar='N', help='number of workers (default: 4)')
+parser.add_argument('--model', type=str, default=None, required=True, metavar='MODEL',
+                    help='model name (default: None)')
+
+parser.add_argument('--resume', type=str, default=None, metavar='CKPT',
+                    help='checkpoint to resume training from (default: None)')
+
+parser.add_argument('--epochs', type=int, default=200, metavar='N', help='number of epochs to train (default: 200)')
+parser.add_argument('--save_freq', type=int, default=25, metavar='N', help='save frequency (default: 25)')
+parser.add_argument('--eval_freq', type=int, default=5, metavar='N', help='evaluation frequency (default: 5)')
+parser.add_argument('--lr_init', type=float, default=0.1, metavar='LR', help='initial learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M', help='SGD momentum (default: 0.9)')
+parser.add_argument('--wd', type=float, default=1e-4, help='weight decay (default: 1e-4)')
+
+#parser.add_argument('--swa', action='store_true', help='swa usage flag (default: off)')
+parser.add_argument('--swa_start', type=int, default=161, metavar='N', help='SWA start epoch number (default: 161)')
+parser.add_argument('--swa_c', type=int, default=20, metavar='N', help='fast-SWA LR schedule cycle length (default: 20)')
+
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+
+args = parser.parse_args()
+
+print('Preparing directory %s' % args.dir)
+os.makedirs(args.dir, exist_ok=True)
+with open(os.path.join(args.dir, 'command.sh'), 'w') as f:
+    f.write(' '.join(sys.argv))
+    f.write('\n')
+
+torch.backends.cudnn.benchmark = True
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+
+print('Using model %s' % args.model)
+model_cfg = getattr(models, args.model)
+
+print('Loading dataset %s from %s' % (args.dataset, args.data_path))
+ds = getattr(torchvision.datasets, args.dataset)
+path = os.path.join(args.data_path, args.dataset.lower())
+train_set = ds(path, train=True, download=True, transform=model_cfg.transform_train)
+test_set = ds(path, train=False, download=True, transform=model_cfg.transform_test)
+loaders = {
+    'train': torch.utils.data.DataLoader(
+        train_set,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.num_workers,
+        pin_memory=True
+    ),
+    'test': torch.utils.data.DataLoader(
+        test_set,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.num_workers,
+        pin_memory=True
+    )
+}
+num_classes = max(train_set.train_labels) + 1
+
+print('Preparing model')
+model = model_cfg.base(*model_cfg.args, num_classes=num_classes, **model_cfg.kwargs)
+model.cuda()
+
+def schedule(epoch):
+    max_step = args.epochs + argc.swa_c
+    t = epoch
+    if t >= max_step:
+        t = max_step - args.c +  (t - max_step) % args.c
+    return (args.lr_init) * (1 + math.cos(math.pi * t / max_step)) / 2
+
+
+criterion = F.cross_entropy
+optimizer = torch.optim.SGD(
+    model.parameters(),
+    lr=args.lr_init,
+    momentum=args.momentum,
+    weight_decay=args.wd
+)
+
+if args.swa:
+    # SWA: initialize SWA optimizer wrapper
+    print('SWA training')
+    steps_per_epoch = len(loaders['train'].dataset) / args.batch_size
+    steps_per_epoch = int(steps_per_epoch)
+    print(f"Steps per epoch: {steps_per_epoch}")
+    optimizer = SWA(optimizer, swa_start=args.swa_start * steps_per_epoch,
+                    swa_freq=steps_per_epoch)
+else:
+    print('SGD training')
+
+start_epoch = 0
+if args.resume is not None:
+    print('Resume training from %s' % args.resume)
+    checkpoint = torch.load(args.resume)
+    start_epoch = checkpoint['epoch']
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+columns = ['ep', 'lr', 'tr_loss', 'tr_acc', 'te_loss', 'te_acc', 'time']
+if args.swa:
+    columns = columns[:-1] + ['swa_te_loss', 'swa_te_acc'] + columns[-1:]
+    swa_res = {'loss': None, 'accuracy': None}
+
+utils.save_checkpoint(
+    args.dir,
+    start_epoch,
+    state_dict=model.state_dict(),
+    optimizer=optimizer.state_dict()
+)
+
+for epoch in range(start_epoch, args.epochs):
+    time_ep = time.time()
+
+    lr = schedule(epoch)
+    utils.adjust_learning_rate(optimizer, lr)
+    train_res = utils.train_epoch(loaders['train'], model, criterion, optimizer)
+    if epoch == 0 or epoch % args.eval_freq == args.eval_freq - 1 or epoch == args.epochs - 1:
+        test_res = utils.eval(loaders['test'], model, criterion)
+    else:
+        test_res = {'loss': None, 'accuracy': None}
+
+    if args.swa and (epoch + 1) >= args.swa_start:
+        #utils.moving_average(swa_model, model, 1.0 / (swa_n + 1))
+        #swa_n += 1
+        if epoch == 0 or epoch % args.eval_freq == args.eval_freq - 1 or epoch == args.epochs - 1:
+            # Batchnorm update
+            optimizer.swap_swa_sgd()
+            bn_update(loaders['train'], model)
+            swa_res = utils.eval(loaders['test'], model, criterion)
+            optimizer.swap_swa_sgd()
+        else:
+            swa_res = {'loss': None, 'accuracy': None}
+
+    if (epoch + 1) % args.save_freq == 0:
+        utils.save_checkpoint(
+            args.dir,
+            epoch + 1,
+            state_dict=model.state_dict(),
+            optimizer=optimizer.state_dict()
+        )
+
+    time_ep = time.time() - time_ep
+    values = [epoch + 1, lr, train_res['loss'], train_res['accuracy'], test_res['loss'], test_res['accuracy'], time_ep]
+    if args.swa:
+        values = values[:-1] + [swa_res['loss'], swa_res['accuracy']] + values[-1:]
+    table = tabulate.tabulate([values], columns, tablefmt='simple', floatfmt='8.4f')
+    if epoch % 40 == 0:
+        table = table.split('\n')
+        table = '\n'.join([table[1]] + table)
+    else:
+        table = table.split('\n')[2]
+    print(table)
+
+if args.epochs % args.save_freq != 0:
+    utils.save_checkpoint(
+        args.dir,
+        args.epochs,
+        state_dict=model.state_dict(),
+        optimizer=optimizer.state_dict()
+    )
