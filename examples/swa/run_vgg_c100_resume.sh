b18dc921e4aa9f1eabeef5a632e39c06004f7e91
Pavel Izmailov <izmailovpavel@gmail.com>
2018-07-31 16:44:45 +0300

    remove .sh files

--- examples/swa/run_vgg_c100_resume.sh
+++
@@ -1,2 +0,0 @@
-python3 train.py --dir=/scratch/pi49/pt_swa/vgg/c100/run1_resume --dataset=CIFAR100 --data_path=/scratch/datasets/ --model=VGG16 --epochs=300 --lr_init=0.05 --wd=5e-4 --swa --swa_start=161 --swa_lr=0.01 --resume=/home/pi49/from_scratch/pt_swa/vgg/c100/run5/checkpoint-250.pt
-mv /scratch/pi49/pt_swa/vgg/c100/run1_resume ~/from_scratch/pt_swa/vgg/c100
--- examples/swa/run_wrn_c100.sh
+++
@@ -1,4 +0,0 @@
-python3 train.py --dir=/scratch/pi49/pt_swa/wrn/c100/run4 --dataset=CIFAR100 --data_path=/scratch/datasets/ --model=WideResNet28x10 --epochs=300 --lr_init=0.1 --wd=5e-4 --swa --swa_start=161 --swa_lr=0.05
-mv /scratch/pi49/pt_swa/wrn/c100/run4 ~/from_scratch/pt_swa/wrn/c100
-python3 train.py --dir=/scratch/pi49/pt_swa/wrn/c100/run5 --dataset=CIFAR100 --data_path=/scratch/datasets/ --model=WideResNet28x10 --epochs=300 --lr_init=0.1 --wd=5e-4 --swa --swa_start=161 --swa_lr=0.05
-mv /scratch/pi49/pt_swa/wrn/c100/run5 ~/from_scratch/pt_swa/wrn/c100
